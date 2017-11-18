27a96f9ee9bad370e74e7921c865e3d4c7dd1c53
Honey Chawla <honeychawla96@gmail.com>
2017-11-18 16:25:56 -0500

    Connected joystick to user movement, enemies only move forward

--- game.ino
+++ game.ino
@@ -26,6 +26,14 @@ int matrix2Screen[8][8] = { {0,0,0,0,0,0,0,0},
                             {0,0,0,0,0,0,0,0},
                             {0,0,0,0,0,0,0,0} };
 
+// Player variables
+int playerX = 0;
+int playerY = 0;
+
+// Gun variables
+int gunX = 0;
+int gunY = 0;
+
 // Joystick
 const int joystickXAxis = A1;
 const int joystickYAxis = A0;
@@ -34,16 +42,6 @@ int xReading;
 int yReading;
 int shoot;
 String dirToMove;
-
-// Player variables
-int playerX = 0;
-int playerY = 0;
-int playerMatrix = 0;
-
-// Gun variables
-int gunX = 0;
-int gunY = 0;
-int gunMatrix = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -71,21 +69,21 @@ void setup() {
 }
 
 void loop() {
-
   boolean playing = true;
 
-  while (playing) {
+  while (playing) {    
     // Generate some enemies
     generateEnemies();
-    delay(500);
+    
+    drawMatrix1();
+    drawMatrix2();
+    delay(300);
   
     // Move enemies
     moveEnemies();
 
-    drawMatrix1();
-    drawMatrix2();
-    
-    delay(random(1800,2800));
+    movePlayer();
+    delay(random(1400,2000));
   }
 }
 
@@ -94,29 +92,23 @@ void movePlayer() {
   yReading = analogRead(joystickYAxis);
   int newPlayerX = playerX;
   int newPlayerY = playerY;
-  int newPlayerMatrix = playerMatrix;
   int newGunX = gunX;
   int newGunY = gunY;
-  int newGunMatrix = gunMatrix;
 
   // Determine which way the player is trying to move
   if (xReading < 200) {
     dirToMove = "LEFT";
-    if (playerMatrix == 1 and playerX == 0) {
+    if (playerX == 0) {
+      // Already at the leftmost column
       newPlayerX = playerX;
-    } else if (playerMatrix == 2 && playerX == 0) {
-      newPlayerMatrix = 1;
-      newPlayerX = 7;
     } else {
       newPlayerX--;
     }
   } else if (xReading > 900) {
     dirToMove = "RIGHT";
-    if (playerMatrix == 2 and playerX == 7) {
+    if (playerX == 6) {
+      // The player can't move to the 7th column because we need room for their weapon
       newPlayerX = playerX;
-    } else if (playerMatrix == 1 && playerX == 7) {
-      newPlayerMatrix = 2;
-      newPlayerX = 0;
     } else {
       newPlayerX++;
     }
@@ -138,120 +130,57 @@ void movePlayer() {
     dirToMove = "STAY";
   }
 
-  // What would the gun's new position be?
-  if (newPlayerMatrix == 1 && newPlayerX == 7) {
-    newGunMatrix = 2;
-    newGunX = 0;
-    newGunY = newPlayerY;
-  } else if (newPlayerMatrix == 2) {
-      gunX = newPlayerX + 1;
-      gunY = newPlayerY;
+  // Determine the gun's new position
+  newGunX = newPlayerX + 1;
+  newGunY = newPlayerY;
+
+  // Move the player and gun to their new location
+
+  if (matrix1Screen[newPlayerX][newPlayerY] == 0 || matrix1Screen[newPlayerX][newPlayerY] == 2) {
+    matrix1Screen[playerX][playerY] = 0;
+    matrix1Screen[gunX][gunY] = 0;
+    matrix1Screen[newPlayerX][newPlayerY] = 1;
+    matrix1Screen[newGunX][newGunY] = 2;
+  } else if (matrix1Screen[newPlayerX][newPlayerY] == 3) {
+    // Moving to where an enemy is will kill me and will end the game
+    matrix1Screen[playerX][playerY] = 0;
+    matrix1Screen[gunX][gunY] = 0;
+    gameOver();
   }
 
-  // Now that we know what matrix we're going to be on and our new position, check if that position is open to move to
-  if (newPlayerMatrix == 1) {
-    if (matrix1Screen[newPlayerX][newPlayerY] == 0) {
-      // There is absolutely nothing there, so we're good.
-      // Have to move the player and the gun.
-    } else if (matrix1Screen[newPlayerX][newPlayerY] == 2) {
-      // My gun is there, it also needs to move tho
-    } else if (matrix1Screen[newPlayerX][newPlayerY] == 3) {
-      // I want to move where an enemy is
-    }
-  } else if (newPlayerMatrix == 2) {
-    
-  }
+  playerX = newPlayerX;
+  playerY = newPlayerY;
+  gunX = newGunX;
+  gunY = newGunY;
+  
+  drawMatrix1();
+  drawMatrix2();
 }
 
 void moveEnemies() {
   for (int i = 0; i < 8; i++) {
-    Serial.println(i);
     for (int j = 0; j < 8; j++) {
-      
-      //Looping through matrix 1
       if (i == 0 && matrix1Screen[i][j] == 3) {
         matrix1Screen[i][j] = 0;
-      } else if (matrix1Screen[i][j] == 3 || matrix2Screen[i][j] == 3) {
-        // We found an enemy! What direction should we move them in?
-        // FORWARD = 0 FORWARDUP = 1 FORWARDDOWN = 2
-        int val = random(3);
-        String dir = "";
-        
-        if (val == 0) {
-          dir = "FORWARD";
-        } else if (val == 1) {
-          dir = "FORWARDUP";
-        } else if (val == 2) {
-          dir = "FORWARDDOWN";
+      } else if (matrix1Screen[i][j] == 3) {
+        if (matrix1Screen[i-1][j] == 0) {
+          // If the spot is open, turn off the current spot and turn on the forward spot
+          matrix1Screen[i][j] = 0;
+          matrix1Screen[i-1][j] = 3;
         }
-
-        // Now that we know what direction to move, let's check if that spot is open in the right matrix
-        if (matrix1Screen[i][j] == 3) {
-          if (dir.equals("FORWARD")) {
-            if (matrix1Screen[i-1][j] == 0) {
-              // The spot is open
-              // Turn off the current spot and turn on the next spot
-              matrix1Screen[i][j] = 0;
-              matrix1Screen[i-1][j] = 3;
-            }
-          } else if (dir.equals("FORWARDUP")) {
-            if (matrix1Screen[i-1][j-1] == 0) {
-              // The spot is open
-              // Turn off the current spot and turn on the next spot
-              matrix1Screen[i][j] = 0;
-              matrix1Screen[i-1][j-1] = 3;
-            }
-          } else if (dir.equals("FORWARDDOWN")) {
-            if (matrix1Screen[i-1][j+1] == 0) {
-              // The spot is open
-              // Turn off the current spot and turn on the next spot
-              matrix1Screen[i][j] = 0;
-              matrix1Screen[i-1][j+1] = 3;
-            }
+      } else if (matrix2Screen[i][j] == 3) {
+        if (i == 0) {
+          // Moving to matrix 1
+          if (matrix1Screen[7][j] == 0) {
+            matrix2Screen[i][j] = 0;
+            matrix1Screen[7][j] = 3;
           }
-        } else if (matrix2Screen[i][j] == 3) {
-            if (i == 0) {
-              // Have to move to matrix 1
-              if (dir.equals("FORWARD")){
-                if (matrix1Screen[7][j] == 0) {
-                  matrix2Screen[i][j] = 0;
-                  matrix1Screen[7][j] = 3;
-                }
-              } else if (dir.equals("FORWARDUP")) {
-                if (matrix1Screen[7][j-1] == 0) {
-                  matrix2Screen[i][j] = 0;
-                  matrix1Screen[7][j-1] = 3;
-                }
-              } else if (dir.equals("FORWARDDOWN")) {
-                if (matrix1Screen[7][j+1] == 0) {
-                  matrix2Screen[i][j] = 0;
-                  matrix1Screen[7][j+1] = 3;
-                }
-              }
-            } else {
-                if (dir.equals("FORWARD")) {
-                  if (matrix2Screen[i-1][j] == 0) {
-                    // The spot is open
-                    // Turn off the current spot and turn on the next spot
-                    matrix2Screen[i][j] = 0;
-                    matrix2Screen[i-1][j] = 3;
-                  }
-                } else if (dir.equals("FORWARDUP")) {
-                    if (matrix2Screen[i-1][j-1] == 0) {
-                      // The spot is open
-                      // Turn off the current spot and turn on the next spot
-                      matrix2Screen[i][j] = 0;
-                      matrix2Screen[i-1][j-1] = 3;
-                    }
-                } else if (dir.equals("FORWARDDOWN")) {
-                    if (matrix2Screen[i-1][j+1] == 0) {
-                      // The spot is open
-                      // Turn off the current spot and turn on the next spot
-                      matrix2Screen[i][j] = 0;
-                      matrix2Screen[i-1][j+1] = 3;
-                    }
-                }
-            }
+        } else if (matrix2Screen[i-1][j] == 0) {
+          if (matrix2Screen[i-1][j] == 0) {
+            // If the spot is open, turn off the current spot and turn on the forward spot
+            matrix2Screen[i][j] = 0;
+            matrix2Screen[i-1][j] = 3;
+          }
         }
       }
     }
@@ -259,9 +188,6 @@ void moveEnemies() {
 }
 
 void generatePlayer() {
-  // The player will always be on matrix 1
-  playerMatrix = 1;
-
   // The x location of the player should be between 0 and 6 so as to leave a spot on the right side for the player's weapon
   playerX = random(7);
   // The y location can be any spot
@@ -275,9 +201,6 @@ void generateGun() {
   // The gun should be generated on the pixel directly to the right of the player
   gunX = playerX + 1;
   gunY = playerY;
-
-  // The matrix that the gun is on is the same as the player upon generation
-  gunMatrix = 1;
 
   // Update the matrices screens
   matrix1Screen[gunX][gunY] = 2;
@@ -302,6 +225,17 @@ void generateEnemies() {
         conflict = false;
       }
     }
+  }
+}
+
+void gameOver() {
+  matrix1.clear();
+  matrix2.clear();
+  matrix1.writeDisplay();
+  matrix2.writeDisplay();
+
+  while (true) {
+    
   }
 }
 
